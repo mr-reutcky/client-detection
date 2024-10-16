782a3b8aa9d55ef85e9ce0a0b761b3450b662164
Samuel Reutcky <sreutcky@gmail.com>
2024-10-16 14:33:40 -0500

    Made the window section work

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,24 @@ function select(selector, scope = document) {
   return scope.querySelector(selector);
 }
 
-function listen(event, selector, callback){
+function listen(event, selector, callback) {
   return selector.addEventListener(event, callback);
 }
+
+const pageWidth = select('.width');
+const pageHeight = select('.height');
+const orient = select('.orient'); // Uncaught SyntaxError: Identifier 'orient' has already been declared
+
+function windowInfo() {
+  pageWidth.innerText = `${window.innerWidth}px`;
+  pageHeight.innerText = `${window.innerHeight}px`;
+  orient.innerText =
+    window.innerWidth >= window.innerHeight ? 'landscape' : 'portrait';
+}
+
+listen('load', window, () => {
+  windowInfo();
+});
+listen('resize', window, () => {
+  windowInfo();
+});
